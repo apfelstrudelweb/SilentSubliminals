34b1df979e4cf315eb052a6fa4e139cab1e02349
Ullrich Vormbrock <ulrich.vormbrock@gmail.com>
2020-05-10 17:44:14 +0200

    Prototype: initial commit

--- SilentSubliminals/ViewController.swift
+++ SilentSubliminals/ViewController.swift
@@ -7,12 +7,38 @@
 //
 
 import UIKit
+import AudioKit
+
+let modulationFrequency: Double = 15592
+let bandwidth: Double = 2000
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        guard let audiofile = try? AKAudioFile(readFileName: "affirmation_01.mp3") else { return }
+        let player = AKPlayer(audioFile: audiofile)
+        let booster = AKBooster(player)
+        
+        let modulatedPlayer = AKOperationEffect(booster) { booster, _ in
+            let sine = AKOperation.sineWave(frequency: modulationFrequency)
+            let modulation = booster * sine
+
+            return modulation
+        }
+        
+        let filter = AKBandPassButterworthFilter(modulatedPlayer, centerFrequency: modulationFrequency + 0.5 * bandwidth, bandwidth: bandwidth)
+
+        AudioKit.output = filter
+        
+        do {
+            try AudioKit.start()
+        } catch {
+            AKLog("AudioKit did not start!")
+        }
+        
+        player.play()
     }
 
 
